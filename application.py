db3dc58b7edcf460a3a7ccfac75e6068d58cc5a1
Juriaan Wagenaar <jwzakelijk99@gmail.com>
2020-01-17 10:36:49 +0000

    app meme

--- application.py
+++ application.py
@@ -33,7 +33,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure CS50 Library to use SQLite database
-db = SQL("sqlite:///finance.db")
+db = SQL("sqlite:///meme.db")
 
 
 @app.route("/")
@@ -141,8 +141,6 @@ def register():
         return redirect("/")
     else:
         return render_template("register.html")
-
-
 
 def errorhandler(e):
     """Handle error"""

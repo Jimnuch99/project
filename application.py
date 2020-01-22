bca4cd1c3a89ffcfc3f36c63bc6a695140a82b76
Tommy van de Logt <you@example.com>
2020-01-22 12:20:39 +0000

    redirects bijgewerkt naar feed

--- application.py
+++ application.py
@@ -6,16 +6,13 @@ from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
+from werkzeug.utils import secure_filename
 
 from helpers import apology, login_required
 
-# Configure application
 app = Flask(__name__)
 
-# Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
-
-# Ensure responses aren't cached
 @app.after_request
 def after_request(response):
     response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
@@ -23,22 +20,51 @@ def after_request(response):
     response.headers["Pragma"] = "no-cache"
     return response
 
-
-# Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-app.config["IMAGE_UPLOADS"] = "/mnt/c/wsl/projects/pythonise/tutorials/flask_series/app/app/static/img/uploads"
-app.config["ALLOWED_IMAGE_EXTENSIONS"] = ["JPEG", "JPG", "PNG", "GIF"]
-app.config["MAX_IMAGE_FILESIZE"] = 0.5 * 1024 * 1024
-
 # Configure CS50 Library to use SQLite database
 db = SQL("sqlite:///meme.db")
 
-
 @app.route("/", methods=["GET", "POST"])
+def register():
+    """Register user"""
+    if request.method == "POST":
+
+        # ensure username was submitted
+        if not request.form.get("username"):
+            return apology("Must provide username")
+
+        # ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("Must provide password")
+
+        # ensure password and verified password is the same
+        elif request.form.get("password") != request.form.get("confirmation"):
+            return apology("password doesn't match")
+
+        # insert the new user into users, storing the hash of the user's password
+        result = db.execute("INSERT INTO users (username, hash) \
+                             VALUES(:username, :hash)", \
+                             username=request.form.get("username"), \
+                             hash=generate_password_hash(request.form.get("password")))
+
+        if not result:
+            return apology("Username already exist")
+
+        # remember which user has logged in
+        session["user_id"] = result
+
+        # redirect user to home page
+        return redirect("/login")
+
+    else:
+        return render_template("register.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
 def login():
     """Log user in"""
 
@@ -68,12 +94,16 @@ def login():
         session["user_id"] = rows[0]["id"]
 
         # Redirect user to home page
-        return redirect("/")
+        return redirect("feed")
 
     # User reached route via GET (as by clicking a link or via redirect)
     else:
-        return render_template("register.html")
+        return render_template("login.html")
 
+@app.route("/feed")
+@login_required
+def feed():
+    return render_template("feed.html")
 
 @app.route("/logout")
 def logout():
@@ -85,48 +115,6 @@ def logout():
     # Redirect user to login form
     return redirect("/")
 
-@app.route("/register", methods=["GET", "POST"])
-@login_required
-def register():
-    """Register user"""
-
-    if request.method == "POST":
-
-        #vraag de velden in de register.html op
-        gebruikersnaam = request.form.get('username')
-        wachtwoord = request.form.get('password')
-        dubbelcheck = request.form.get('confirmation')
-        wachtwoordhash = generate_password_hash(wachtwoord)
-
-        #de foutmeldingen wanneer een van de velden niet is ingevoerd
-        if not gebruikersnaam:
-            return apology("Voer een gebruikersnaam in")
-
-        elif not wachtwoord:
-            return apology('Voer een wachtwoord in')
-
-        elif not dubbelcheck:
-            return apology('Voer een wachtwoord in')
-
-        #geef een foutmelding als de opgegeven wachtwoorden niet overeenkomen
-        elif dubbelcheck != wachtwoord:
-            return apology('Wachtwoorden komen niet overeen')
-
-        #als alles is goedgegaan, voeg de gebruiker toe in de user-database
-        database = db.execute("SELECT * FROM users WHERE username =:username", username = gebruikersnaam)
-
-        if gebruikersnaam in database:
-            return apology("Gebruikersnaam is al in gebruik")
-
-        else:
-            db.execute("INSERT INTO users (username, hash) VALUES (:username, :hash)",username=gebruikersnaam, hash=wachtwoordhash)
-
-        #session["user_id"] = goedgekeurd
-        flash("Welkom bij memestagram!")
-        return redirect("/")
-
-    else:
-        return render_template("register.html")
 
 
 def errorhandler(e):
